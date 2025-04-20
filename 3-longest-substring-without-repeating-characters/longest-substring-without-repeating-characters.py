00b4c0d80a28a7d0f2ecc3d61ce994cd2729ded0
Rithvik Rajavelu <43206993+rithraj@users.noreply.github.com>
2025-04-20 17:35:13 -0400

    Time: 15 ms (81.86%) | Memory: 17.6 MB (96.94%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,17 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_index = {}  # HashMap to store the last seen index of characters
-        left = 0  # Left pointer of the sliding window
-        max_length = 0
+        charSet = set()
+        l = 0
+        res = 0
 
-        for right in range(len(s)):
-            if s[right] in char_index:
-                left = max(char_index[s[right]] + 1, left)  # Move left past the duplicate
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
             
-            char_index[s[right]] = right  # Update the last seen index
-            max_length = max(max_length, right - left + 1)  # Update max length
-
-        return max_length
 
 
         
