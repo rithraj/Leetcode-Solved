5f1f0f01580d8ffa596e6dc6acd08d98856d1b90
Rithvik Rajavelu <43206993+rithraj@users.noreply.github.com>
2025-07-30 17:05:33 -0400

    Time: 7 ms (46.03%) | Memory: 18.9 MB (93.20%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,22 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        adjacency = {}
+        adj = {}
         indeg = [0] * numCourses
-        for c in range(numCourses):
-            adjacency[c] = []
-        for c, req in prerequisites:
-                adjacency[req].append(c)
-                indeg[c] += 1
 
+        for c in range(numCourses):
+            adj[c] = []
+
+        for c, req in prerequisites:
+            adj[req].append(c)
+            indeg[c] +=1
+        
         q = deque([i for i in range(numCourses) if indeg[i] == 0])
         taken = 0
         while q:
-            cur = q.popleft()
+            course = q.popleft()
             taken += 1
 
-            for nxt in adjacency[cur]:
+            for nxt in adj[course]:
                 indeg[nxt] -= 1
                 if indeg[nxt] == 0:
                     q.append(nxt)
+
         return taken == numCourses
+        
