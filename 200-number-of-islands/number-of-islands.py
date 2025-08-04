b38ff5474755345c03ebabbaf1c6a76846675006
Rithvik Rajavelu <43206993+rithraj@users.noreply.github.com>
2025-08-04 09:28:04 -0400

    Time: 266 ms (34.18%) | Memory: 25.3 MB (30.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
         visited = set()
-        m = len(grid)
-        n = len(grid[0])
-        count = 0
+
         def dfs(i, j):
-            directions = [[0,1],[0,-1],[1,0],[-1,0]]
             stack = []
+            visited.add((i,j))
             stack.append((i,j))
             while stack:
-                i,j = stack.pop()
-                for dx,dy in directions:
-                    if i + dx >= 0 and i + dx < m and j + dy >= 0 and j + dy < n and grid[i+dx][j+dy] == "1" and (i+dx, j + dy) not in visited:
-                        stack.append((i+dx, j+dy))
-                        visited.add((i+dx,j+dy))
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    dfs(i,j)
+                x,y = stack.pop()
+                for dx, dy in directions:
+                    r, c = dx + x, dy + y
+                    if (r,c) not in visited and 0<=r<rows and 0<=c<cols and grid[r][c] == "1":
+                        stack.append((r,c))
+                        visited.add((r,c))
+
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in visited and 0<=r<rows and 0<=c<cols and grid[r][c] == "1":
+                    dfs(r,c)
                     count += 1
+
+
         return count
